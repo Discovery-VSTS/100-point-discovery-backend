00aca01d9e6e8ac7629912974988d561f2dd0691
Ricard Gascons <gascons1995@gmail.com>
2017-01-29 22:52:42 +0000

    Add Member model test case

--- pointdistribution/core/tests.py
+++ pointdistribution/core/tests.py
@@ -1,1 +1,10 @@
 from django.test import TestCase
+
+
+from .models import Member
+
+
+class MemberModelTest(TestCase):
+    def test_string_representation(self):
+        entry = Member(name="My entry title")
+        self.assertEqual(str(entry), entry.name)
