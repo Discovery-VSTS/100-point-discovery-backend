c5be94dfaababd123f457cf4dd0d045ddbbead5a
Ricard Gascons <gascons1995@gmail.com>
2017-02-20 12:01:08 +0000

    Update given points from PUT endpoint

--- pointdistribution/core/serializers.py
+++ pointdistribution/core/serializers.py
@@ -29,6 +29,9 @@ class PointDistributionSerializer(serializers.ModelSerializer):
     def update(self, instance, validated_data):
         given_points_data = validated_data.pop('given_points')
         for given_point_data in given_points_data:
-            GivenPoint.objects.create(point_distribution=instance, **given_point_data)
+            points = given_point_data['points']
+            del given_point_data['points']
+            GivenPoint.objects.update_or_create(defaults={'points': points},
+                                                point_distribution=instance, **given_point_data)
         instance.save()
         return instance
