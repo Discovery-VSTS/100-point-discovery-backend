e9130bff48921e3b8651621b18369ae5ef08e5d9
Ricard Gascons <gascons1995@gmail.com>
2017-03-12 22:22:02 +0000

    Switching to postgres

--- pointdistribution/pointdistribution/settings.py
+++ pointdistribution/pointdistribution/settings.py
@@ -149,7 +149,7 @@ if 'TRAVIS' in os.environ:
 elif 'PROD' in os.environ:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.mysql',
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': os.environ['DB_NAME'],
             'USER': os.environ['DB_USER'],
             'PASSWORD': os.environ['DB_PWD'],
