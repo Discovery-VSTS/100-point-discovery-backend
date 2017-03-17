19a0edcb6fd4332ca6b44f10c6783ac3bcb11b76
Minh-Long Do <minhlong.langos@gmail.com>
2017-03-17 15:29:00 +0000

    Add endpoint for retrieving all teams and a single team with all of its members, create team when fetching members

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     url(r'members/$', views.MemberList.as_view()),
+    url(r'teams/team/$', views.TeamList.as_view()),
+    url(r'teams/all$', views.TeamList.as_view()),
     url(r'members/reset/$', views.reset_database),
     url(r'member/history/(?P<email>.+)/$', views.MemberPointsHistory.as_view()),
     url(r'points/distribution/(?P<week>\d{4}-\d{2}-\d{2})/$', views.PointDistributionWeek.as_view()),
